f920d8a812bf86b1d4e849d107f9899a705ec516
1587precious <preciousconstance7@gmail.com>
2024-04-20 15:48:53 -0700

    task 1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-""" Fabfile that create a .tgz archive from
-the contents of web_static folder"""
+"""
+A fabric script that generates a .tgz archive from the contents of the
+web_static folder of your AirBnB Clone repo, using the function do_pack
+"""
 
-# if __name__ == '__main__':
-from fabric.api import local
+from os import path
 from datetime import datetime
+from fabric.api import local
 
 
 def do_pack():
-    """Pack all the contents in the web_static directory
-    as a tar archive"""
-
-    try:
-        local("mkdir -p versions")
-        time = datetime.now()
-        date_string = '%Y%m%d%H%M%S'
-        date = time.strftime(date_string)
-
-        file_path = "versions/web_static_{}.tgz".format(date)
-        local("tar -czvf {} web_static".format(file_path))
-        return file_path
-
-    except Exception:
-        return None
+    """
+    A function that generates an archive
+    """
+    date = datetime.now()
+    archive = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        date.year, date.month, date.day,
+        date.hour, date.minute, date.second
+    )
+    if not path.isdir("versions"):
+        if local("mkdir versions").failed:
+            return None
+    cmd = "cd web_static && tar -cvzf ../{} . && cd -".format(archive)
+    if local(cmd).succeeded:
+        return archive
+    return None
