3160dba9c59b07471dd2ce86db52ce9020634ce3
1587precious <preciousconstance7@gmail.com>
2024-05-06 14:29:17 -0700

    clone

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,68 +1,20 @@
-import unittest
-import MySQLdb
-from models import storage
-from models.state import State
-from console import HBNBCommand
-from io import StringIO
-from unittest.mock import patch
+#!/usr/bin/python3
+""" This module contains the function do_pack that generates a .tgz archive
+  from the contents of the web_static folder (fabric script) """
 
-class TestMySQLStorage(unittest.TestCase):
-    
-    @classmethod
-    def setUpClass(cls):
-        """Set up MySQL connection and create necessary tables."""
-        cls.connection = MySQLdb.connect(host='localhost',
-                                          user='hbnb_test',
-                                          passwd='hbnb_test_pwd',
-                                          db='hbnb_test_db',
-                                          charset='utf8')
-        cls.cursor = cls.connection.cursor()
-        cls.cursor.execute("DROP TABLE IF EXISTS states")
-        cls.cursor.execute("CREATE TABLE states (id VARCHAR(60) NOT NULL, name VARCHAR(128), PRIMARY KEY (id))")
-        cls.connection.commit()
-        cls.connection.close()
 
-    @classmethod
-    def tearDownClass(cls):
-        """Close MySQL connection."""
-        cls.connection.close()
+from fabric.api import *
+from datetime import datetime
 
-    def setUp(self):
-        """Set up a clean database before each test."""
-        self.connection = MySQLdb.connect(host='localhost',
-                                           user='hbnb_test',
-                                           passwd='hbnb_test_pwd',
-                                           db='hbnb_test_db',
-                                           charset='utf8')
-        self.cursor = self.connection.cursor()
-        self.cursor.execute("DELETE FROM states")
-        self.connection.commit()
 
-    def tearDown(self):
-        """Close MySQL connection after each test."""
-        self.connection.close()
-
-    def test_create_state_command(self):
-        """Test if the create State command adds a new record in the states table."""
-        # Get initial count of records in the states table
-        initial_count = self._get_record_count()
-
-        # Execute the console command
-        with patch('sys.stdout', new=StringIO()) as fake_out:
-            HBNBCommand().onecmd("create State name=\"California\"")
-
-        # Get count of records in the states table after executing the command
-        final_count = self._get_record_count()
-
-        # Check if the difference is +1
-        self.assertEqual(final_count - initial_count, 1)
-
-    def _get_record_count(self):
-        """Helper method to get the number of records in the states table."""
-        self.cursor.execute("SELECT COUNT(*) FROM states")
-        count = self.cursor.fetchone()[0]
-        return count
-
-if __name__ == '__main__':
-    unittest.main()
-
+def do_pack():
+    """ Fabric script that generates a .tgz archive from the contents of the...
+    ...web_static folder """
+    local("sudo mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("sudo tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
+    else:
+        return None
